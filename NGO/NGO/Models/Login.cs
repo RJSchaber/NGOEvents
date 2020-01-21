a95e1b2db1e9f24bed7132ec7b2224cf96355535
fengli738 <fli19@hawk.iit.edu>
2020-01-21 12:34:56 -0500

    Update1

---
+++ NGO/NGO/Models/Login.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.ComponentModel.DataAnnotations;
+
+namespace NGO.Models
+{
+    public class Login
+    {
+        [Required(ErrorMessage ="Please Enter your email.")]
+        [Display(Name ="Email: ")]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage ="Please Enter your password.")]
+
+        public string Password { get; set; }
+
+        public string LoginProcess(String stremail, String strpassword)
+        {
+            String message = " ";
+
+        }
+    }
+}
